b288118bdfbba09e21bc6b77c46549ac54ffee10
bartoszz99 <bartosok2008@gmail.com>
2026-04-26 20:54:43 +0200

    Use environment variables for API keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = 'https://kefaloaydyachrccjzuy.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlZmFsb2F5ZHlhY2hyY2NqenV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2OTM5NjYsImV4cCI6MjA5MjI2OTk2Nn0.LdmIQlZhjraO6mho1w-aaYHshn5cztncoU76BsAeBE8';
+const SUPABASE_URL      = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
